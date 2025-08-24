909c839802ab43e1d82ecc11c1de6591636b300d
lilytwelftree <lilytwelftree@gmail.com>
2025-08-24 21:29:51 +0930

    Fix function name to array_mean

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <iomanip>
 
-double average_sum(int array[], int n);
+double array_mean(int array[], int n);
 
 int main() {
     int numbers[] = {1, 2, 3, 4, 7};
     int n = 5;
-    double result = average_sum(numbers,n); 
+    double result = array_mean(numbers,n); 
     std::cout << std::fixed << std::setprecision(1) << "Average: " << result << std::endl;
 }
