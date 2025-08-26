0d6c33690935a7b5d81bb06cc2e9f499cbce6560
lilytwelftree <lilytwelftree@gmail.com>
2025-08-26 10:44:55 +0930

    }Question1

---
+++ function-1-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int sum_diagonal(int array[4][4]){
+
+    int diagonalSum = 0;
+
+    for (int i = 0; i<4; i++){
+        diagonalSum = diagonalSum + array[i][i];
+    }
+
+    std::cout << diagonalSum << std::endl ;
+
+    return diagonalSum;
+}
---
+++ main-1-1.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+
+int sum_diagonal(int array[4][4]);
+
+int main(){
+
+    int array[4][4] = {{5,5,5,5},{5,5,5,5},{5,5,5,5},{5,5,5,5}};
+    sum_diagonal(array);
+
+    return 0;
+}
