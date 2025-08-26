59792b961142e7c76da8c70ea22ed181eb421b4e
lilytwelftree <lilytwelftree@gmail.com>
2025-08-26 11:11:32 +0930

    updated Q2 to check for zeros

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -5,13 +5,21 @@ int is_identity(int array[10][10]){
     bool checkIdentity;
 
     for (int i = 0; i<10; i++){
-        if (array[i][i] != 1){
-            std::cout << "Not an idenity matrix" << std::endl;
-            return 0;
-        } else{
-            continue;
+        for (int j = 0; j<10; j++){
+            if (i == j){
+                if (array[i][j] != 1){
+                    std::cout << "Not an idenity matrix" << std::endl;
+                    return 0;
+                }
+            } else if(array[i][j] != 0){
+                std::cout << "Not an idenity matrix" << std::endl;
+                return 0;
+            } else {
+                continue;
+            }
         }
     }
+
     std::cout << "Yes, this is an idenity matrix" << std::endl;
     return 1;
 }
