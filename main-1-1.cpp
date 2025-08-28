87be33a5a228ed2e6521cd6cd542f1dcbcce35cf
lilytwelftree <lilytwelftree@gmail.com>
2025-08-28 18:23:49 +0930

    re push not working

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -7,7 +7,7 @@ int main(){
     int array[4][4] = {{1,5,5,5},{5,1,5,5},{5,5,1,5},{5,5,5,1}};
     int result = sum_diagonal(array);
 
-    std::cout << result << std::endl;
+    // std::cout << result << std::endl;
 
     return 0;
 }
