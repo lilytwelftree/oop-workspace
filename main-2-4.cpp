8f37cb5043c3ae55308a0daa52f0f61591590054
lilytwelftree <lilytwelftree@gmail.com>
2025-08-31 17:48:52 +0930

    final

--- main-2-4.cpp
+++ main-2-4.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 
-// Function declarations (these would normally be in a header file)
 int array_min(int integers[], int length);
 int array_max(int integers[], int length);
 int sum_min_max(int integers[], int length);
 
 int main() {
-    // Test case 1: Normal array
     int arr1[] = {3, 1, 4, 1, 5, 9, 2, 6};
     int length1 = 8;
     std::cout << "Test 1 [3, 1, 4, 1, 5, 9, 2, 6]:" << std::endl;
