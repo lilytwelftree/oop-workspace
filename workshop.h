f74170c0780bfdf0d14d97869f38f7d5db7ebad8
lilytwelftree <lilytwelftree@gmail.com>
2025-08-31 19:57:05 +0930

    updated to have header file

---
+++ workshop.h
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+// Question 2
+void changeValue(double* ptr);
+
+// Question 3
+void printArray(double* array_ptr, int size);
+
+// Question 4
+double arrayMax(double* array, int size);
+
+// Question 7
+double arrayMax(double* array, int size);
+
+double* dynamicArray(int N, double M);
