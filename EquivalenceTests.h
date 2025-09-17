6204edc87703643b08440e91b0db36d55f0a69b4
lilytwelftree <lilytwelftree@gmail.com>
2025-09-17 23:36:01 +0930

    added

---
+++ EquivalenceTests.h
@@ -0,0 +1,17 @@
+// EquivalenceTests.h
+
+#ifndef EQUIVALENCETESTS_H
+#define EQUIVALENCETESTS_H
+
+#include <iostream>
+
+class EquivalenceTests {
+public:
+    void runTests() {
+        std::cout << "=== Running Equivalence Tests ===" << std::endl;
+        std::cout << "Equivalence tests completed." << std::endl;
+        std::cout << std::endl;
+    }
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 // main.cpp
 
 #include "UnitTest.h"
+#include "EquivalenceTests.h"
 #include "InvoiceTest.h"
 
 int main() {
     UnitTest unitTest;
     unitTest.runTests();
+    
+    EquivalenceTests equivalenceTests;
+    equivalenceTests.runTests();
     
     InvoiceTest invoiceTest;
     invoiceTest.runTests();
